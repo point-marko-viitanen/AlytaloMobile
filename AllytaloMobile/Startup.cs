d60d8dca38bb94b0cd8ce7c25b9be4e0753efb09
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2017-01-30 10:51:26 +0200

    Add project files.

---
+++ AllytaloMobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllytaloMobile.Startup))]
+namespace AllytaloMobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
